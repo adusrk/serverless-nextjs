7c8b04f2e47e1ffdb7adfb16fa8787a606759d01
Adnan Haider <Adussrk@gmail.com>
2024-03-24 18:29:57 +0530

    ui update

--- components/Card.js
+++ components/Card.js
@@ -2,13 +2,10 @@ import React from 'react';
 import styles from './Card.module.css'; // Adjust the path as necessary
 
 const Card = ({data}) => {
-  const cardStyle = {
-    height: data ? 'var(--card-height)' : '200px' // Example: '200px' when no data
-  };
-
+  
   return (
     <>
-    <div className={styles.card} style={cardStyle}>
+    <div className={styles.card}>
       <div className={styles.cardContent}>
     {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Welcome to the server!"}
   </div>
